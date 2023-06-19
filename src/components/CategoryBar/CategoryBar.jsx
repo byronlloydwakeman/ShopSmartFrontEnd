0b764917f903b66e68ce10727352add0bfdb87ca
Byron Lloyd-Wakeman <byzstorm1103@gmail.com>
2023-06-19 10:52:29 +0100

    Moved category bar

--- src/components/CategoryBar/CategoryBar.jsx
+++ src/components/CategoryBar/CategoryBar.jsx
@@ -9,50 +9,57 @@ import "../../App.css";
 import "./CategoryBar.css";
 
 export const CategoryBar = ({waitrose, setWaitrose, asda, setAsda, tesco, setTesco}) => {
-  const [locations, setLocations] = useState([])
-  const [input, setInput] = useState("");
-  const [isFetching, setIsFetching] = useState(true);
-  const [error, setError] = useState(null);
-
+  const [open, setOpen] = useState(false);
+  
   return (
     <div className="flex-column categoryBar-container">
       <div className="flex-row" style={{alignItems: "center"}}>
         <h1 className="categoryBar-text">Supermarket Preference</h1>
+        <img src={require("../../images/downArrow.png")} className='categoryBar-arrow' onClick={() => {
+          setOpen(!open);
+        }}></img>
         <a data-tooltip-id="my-tooltip" data-tooltip-content={`Use the toggles to select/unselect certain supermarkets getting searched.`}>
           ❔
         </a>
         <Tooltip id="my-tooltip" multiline={true}/>
       </div>
-        <div className="flex-column">
-          <div className="flex-row">
-            <h1 className='categoryBar-text'>
-              Waitrose
-            </h1>
-            <div className='toggleBar-container'>
-              <Toggle toggle={waitrose} setToggle={setWaitrose} id={1}/>
-            </div>
-          </div>
-          <div>
+      {
+        open ? (
+          <div className="flex-column">
             <div className="flex-row">
               <h1 className='categoryBar-text'>
-                Asda
+                Waitrose
               </h1>
               <div className='toggleBar-container'>
-                <Toggle toggle={asda} setToggle={setAsda} id={2}/>
+                <Toggle toggle={waitrose} setToggle={setWaitrose} id={1}/>
+              </div>
+            </div>
+            <div>
+              <div className="flex-row">
+                <h1 className='categoryBar-text'>
+                  Asda
+                </h1>
+                <div className='toggleBar-container'>
+                  <Toggle toggle={asda} setToggle={setAsda} id={2}/>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="flex-row">
+                <h1 className='categoryBar-text'>
+                  Tesco
+                </h1>
+                <div className='toggleBar-container'>
+                  <Toggle toggle={tesco} setToggle={setTesco} id={3}/>
+                </div>
               </div>
             </div>
           </div>
-          <div>
-            <div className="flex-row">
-              <h1 className='categoryBar-text'>
-                Tesco
-              </h1>
-              <div className='toggleBar-container'>
-                <Toggle toggle={tesco} setToggle={setTesco} id={3}/>
-              </div>
-            </div>
-          </div>
-        </div>
+        ) : (
+          <></>
+        )
+      }
+
         {/* <div className='flex-column'>
           <h1>Auto search by location</h1>
           <div className="flex-row">
